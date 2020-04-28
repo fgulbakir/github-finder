0a44af7c550883a7133e4d70d5fc6a793da45e7a
fgulbakir <fatmagulbakir61@gmail.com>
2020-04-29 02:50:36 +0300

    github.js updated

--- github.js
+++ github.js
@@ -4,7 +4,7 @@ class Github {
         this.client_secretId = 'a45c7493ca784e17d501c114790aa00afd98e727';
 
     }
-    asyncgetUser(user) {
+    async getUser(user) {
         const profileResponse = await 
         fetch('https://api.github.com/users/${user}?client_id=${this.client_id}&client_secretId=${this.client_secretId}');
 
